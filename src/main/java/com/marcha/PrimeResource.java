80ec6a868157e82003895ede28b4ce88269d737b
root <root@localhost.localdomain>
2025-04-06 00:14:19 -0400

    [fix] micrometer

--- src/main/java/com/marcha/PrimeResource.java
+++ src/main/java/com/marcha/PrimeResource.java
@@ -1,13 +1,26 @@
 package com.marcha;
 
-import jakarta.ws.rs.*;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.marcha.prime.PrimeResponsePojo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 @Path("/prime")
 public class PrimeResource {
+
+    @Inject
+    MeterRegistry registry;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -29,11 +42,19 @@ public class PrimeResource {
     @Produces(MediaType.APPLICATION_JSON)
     public PrimeResponsePojo getPrimes(@QueryParam("limit") int limit) {
         if (limit < 2) {
+            registry.counter("com.marcha.prime.number", "type", "less-than-two").increment();
             Integer count = 0;
             return new PrimeResponsePojo(limit, count, new ArrayList<Integer>());
         }
+        if (limit==10){
+            throw new Error("itotekina error");
+        }
 
+        registry.counter("com.marcha.prime.number", "type", "more-than-two-or-equal").increment();
+
+        Timer.Sample sample = Timer.start(registry);
         List<Integer> primes = sieveOfEratosthenes(limit);
+        sample.stop(registry.timer("com.marcha.prime.number", "type", "list-seconds"));
         Integer count = primes.size();
         return new PrimeResponsePojo(limit, count, primes);
     }
